85464666139c1750b28af1a623dd7448e683f77c
NFDTech <nfdtechsoftware@gmail.com>
2026-08-18 07:57:06 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/nataliafdangelo/votocooperativa/VotoCooperativaApplication.java
@@ -0,0 +1,13 @@
+package br.com.nataliafdangelo.votocooperativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotoCooperativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotoCooperativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nataliafdangelo/votocooperativa/VotoCooperativaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nataliafdangelo.votocooperativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotoCooperativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
