86ad0f13d4fbab5acdd70373df07b1e2d9e67e41
Natalia Ferreira DAngelo <nataliaferreira.dev@yahoo.com>
2026-08-18 16:03:20 -0300

    fix: corrige typo na senha do profile postgres e adiciona @Size em CriarPautaRequest

--- src/main/java/br/com/nataliafdangelo/votocooperativa/dto/CriarPautaRequest.java
+++ src/main/java/br/com/nataliafdangelo/votocooperativa/dto/CriarPautaRequest.java
@@ -1,6 +1,9 @@
 package br.com.nataliafdangelo.votocooperativa.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record CriarPautaRequest(@NotBlank String titulo, String descricao) {
+public record CriarPautaRequest(
+        @NotBlank @Size(max = 255) String titulo,
+        @Size(max = 255) String descricao) {
 }
